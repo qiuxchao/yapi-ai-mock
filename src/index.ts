0349a27257ef731522a74132b11f2617ebd21d0c
qiuxchao <2915241235@qq.com>
2023-07-24 16:07:44 +0800

    🧩 update:

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
 import { a } from './a';
+import ora from 'ora';
+import consola from 'consola';
 
-console.log(a);
+consola.info('Using consola 3.0.0');
+consola.start('Building project...');
+consola.warn('A new version of consola is available: 3.0.1');
+consola.success('Project built!');
+consola.error(new Error('This is an example error. Everything is fine!'));
+consola.box('I am a simple box');
+
+consola.prompt('Deploy to the production?', {
+	type: 'confirm',
+});
+
+const spinner = ora('Loading unicorns').start();
+setTimeout(() => {
+	spinner.color = 'yellow';
+	spinner.text = 'Loading rainbows';
+}, 1000);
