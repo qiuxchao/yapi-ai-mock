da6cd28580afd3c2bd5e4bd6d561d7445a129601
qiuxchao <2915241235@qq.com>
2023-08-23 18:46:51 +0800

    🧩 update: 删除无用代码

--- example/mock.js
+++
@@ -1,9 +0,0 @@
-import Mock from 'mockjs';
-
-const data = Mock.mock({
-	code: 200,
-	message: 'success',
-	'data|10-20': [{ 'itemCategoryId|1-100': 1, itemCateGoryName: '@ctitle(2, 5)' }],
-});
-
-console.log(data);
