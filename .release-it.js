aefddb002f11d25948d7992316db8f0727f8f10f
qiuxchao <2915241235@qq.com>
2023-09-12 11:00:31 +0800

    🐳 chore: changelog

--- .release-it.js
+++ .release-it.js
@@ -1,8 +1,19 @@
 module.exports = {
+  hooks: {
+    'before:release': ['pnpm eslint:fix', 'pnpm build'],
+    'after:release': "echo 'Release completed ✅'",
+  },
   git: {
     commitMessage: '🐳chore: release v${version}',
   },
   github: {
     release: true,
+  },
+  plugins: {
+    '@release-it/conventional-changelog': {
+      preset: 'angular',
+      header: '# Changelog',
+      infile: 'CHANGELOG.md',
+    },
   },
 };
--- build.config.ts
+++ build.config.ts
@@ -3,44 +3,44 @@ import path from 'path';
 import { defineBuildConfig } from 'unbuild';
 
 export default defineBuildConfig({
-	entries: [
-		'src/index',
-		'src/cli',
-		{
-			builder: 'mkdist',
-			input: 'src/chat/typechat',
-			outDir: 'lib/typechat',
-			format: 'cjs',
-			declaration: false,
-		},
-	],
-	externals: ['typechat'],
-	outDir: 'lib',
-	declaration: true,
-	clean: true,
-	rollup: {
-		inlineDependencies: true,
-		emitCJS: true,
-		cjsBridge: true,
-		esbuild: {
-			// minify: true,
-			target: 'node14',
-		},
-		alias: {
-			entries: {
-				'@': path.resolve(__dirname, 'src'),
-			},
-		},
-	},
-	failOnWarn: false,
-	hooks: {
-		'build:done': async context => {
-			copy(path.resolve(__dirname, 'src/assets'), path.resolve(__dirname, 'lib/assets'));
-			const cliCJSContent = readFileSync(path.resolve(__dirname, 'lib/cli.cjs'), 'utf8');
-			writeFileSync(
-				path.resolve(__dirname, 'lib/cli.cjs'),
-				cliCJSContent.replace(/require\('typechat'\)/, `require('./typechat')`),
-			);
-		},
-	},
+  entries: [
+    'src/index',
+    'src/cli',
+    {
+      builder: 'mkdist',
+      input: 'src/chat/typechat',
+      outDir: 'lib/typechat',
+      format: 'cjs',
+      declaration: false,
+    },
+  ],
+  externals: ['typechat'],
+  outDir: 'lib',
+  declaration: true,
+  clean: true,
+  rollup: {
+    inlineDependencies: true,
+    emitCJS: true,
+    cjsBridge: true,
+    esbuild: {
+      minify: true,
+      target: 'node14',
+    },
+    alias: {
+      entries: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+  },
+  failOnWarn: false,
+  hooks: {
+    'build:done': async context => {
+      copy(path.resolve(__dirname, 'src/assets'), path.resolve(__dirname, 'lib/assets'));
+      const cliCJSContent = readFileSync(path.resolve(__dirname, 'lib/cli.cjs'), 'utf8');
+      writeFileSync(
+        path.resolve(__dirname, 'lib/cli.cjs'),
+        cliCJSContent.replace(/require\('typechat'\)/, `require('./typechat')`),
+      );
+    },
+  },
 });
